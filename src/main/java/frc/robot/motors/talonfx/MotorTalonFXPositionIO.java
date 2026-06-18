769ff42edfdd8aaa18bd31573bfc5dacde0b172f
SuppressedCoder <aldwinpham@gmail.com>
2026-06-17 22:53:19 -0400

    Add Indentation

--- src/main/java/frc/robot/motors/talonfx/MotorTalonFXPositionIO.java
+++ src/main/java/frc/robot/motors/talonfx/MotorTalonFXPositionIO.java
@@ -20,72 +20,72 @@ import frc.robot.CanID;
 import frc.robot.motors.MotorPositionClosedLoopIO;
 
 public class MotorTalonFXPositionIO implements MotorPositionClosedLoopIO {
-    protected final TalonFX motor;
-    private final ControlRequest controlRequest;
-    private double targetPositionRotations = 0.0;
+  protected final TalonFX motor;
+  private final ControlRequest controlRequest;
+  private double targetPositionRotations = 0.0;
 
-    public MotorTalonFXPositionIO(CanID can, PositionControlRequest positionControlRequest) {
-        motor          = new TalonFX(can.ID());
-        controlRequest = switch (positionControlRequest) {
-            case DUTYCYCLE -> new PositionDutyCycle(0);
-            case VOLTAGE -> new PositionVoltage(0);
-            case TORQUE -> new PositionTorqueCurrentFOC(0);
-        };
+  public MotorTalonFXPositionIO(CanID can, PositionControlRequest positionControlRequest) {
+    motor = new TalonFX(can.ID());
+    controlRequest = switch (positionControlRequest) {
+      case DUTYCYCLE -> new PositionDutyCycle(0);
+      case VOLTAGE   -> new PositionVoltage(0);
+      case TORQUE    -> new PositionTorqueCurrentFOC(0);
+    };
+  }
+
+  @Override
+  public void setTargetPosition(Angle angle) {
+    if (controlRequest instanceof PositionDutyCycle request) {
+      motor.setControl(request.withPosition(angle));
+    } else if (controlRequest instanceof PositionVoltage request) {
+      motor.setControl(request.withPosition(angle));
+    } else if (controlRequest instanceof PositionTorqueCurrentFOC request) {
+      motor.setControl(request.withPosition(angle));
+    } else {
+      throw new IllegalStateException("Illegal State of PositionControlRequest");
     }
 
-    @Override
-    public void setTargetPosition(Angle angle) {
-        if (controlRequest instanceof PositionDutyCycle request) {
-            motor.setControl(request.withPosition(angle));
-        } else if (controlRequest instanceof PositionVoltage request) {
-            motor.setControl(request.withPosition(angle));
-        } else if (controlRequest instanceof PositionTorqueCurrentFOC request) {
-            motor.setControl(request.withPosition(angle));
-        } else {
-            throw new IllegalStateException("Illegal State of PositionControlRequest");
-        }
+    targetPositionRotations = angle.in(Rotations);
+  }
 
-        targetPositionRotations = angle.in(Rotations);
-    }
+  @Override
+  public void resetEncoder() {
+    motor.setPosition(0);
+  }
 
-    @Override
-    public void resetEncoder() {
-        motor.setPosition(0);
-    }
+  @Override
+  public void setDutyCycle(Dimensionless percent) {
+    double clampedPercent = MathUtil.clamp(percent.in(Value), -1, 1);
+    motor.setControl(new DutyCycleOut(clampedPercent));
+  }
 
-    @Override
-    public void setDutyCycle(Dimensionless percent) {
-        double clampedPercent = MathUtil.clamp(percent.in(Value), -1, 1);
-        motor.setControl(new DutyCycleOut(clampedPercent));
-    }
+  @Override
+  public void setVoltage(Voltage voltage) {
+    double clampedVolts = MathUtil.clamp(voltage.in(Volts), -12, 12);
+    motor.setVoltage(clampedVolts);
+  }
 
-    @Override
-    public void setVoltage(Voltage voltage) {
-        double clampedVolts = MathUtil.clamp(voltage.in(Volts), -12, 12);
-        motor.setVoltage(clampedVolts);
-    }
+  @Override
+  public void stop() {
+    motor.stopMotor();
+  }
 
-    @Override
-    public void stop() {
-        motor.stopMotor();
-    }
+  @Override
+  public void updateInputs(MotorPositionClosedLoopIOInputs inputs) {
+    inputs.connected               = motor.isConnected();
+    inputs.positionRotations       = motor.getPosition().getValueAsDouble();
+    inputs.velocityRPM             = motor.getVelocity().getValue().in(RPM);
+    inputs.voltageVolts            = motor.getMotorVoltage().getValueAsDouble();
+    inputs.currentAmps             = motor.getStatorCurrent().getValueAsDouble();
+    inputs.powerWatts              = inputs.voltageVolts * inputs.currentAmps;
+    inputs.temperatureCelsius      = motor.getDeviceTemp().getValueAsDouble();
 
-    @Override
-    public void updateInputs(MotorPositionClosedLoopIOInputs inputs) {
-        inputs.connected          = motor.isConnected();
-        inputs.positionRotations  = motor.getPosition().getValueAsDouble();
-        inputs.velocityRPM        = motor.getVelocity().getValue().in(RPM);
-        inputs.voltageVolts       = motor.getMotorVoltage().getValueAsDouble();
-        inputs.currentAmps        = motor.getStatorCurrent().getValueAsDouble();
-        inputs.powerWatts         = inputs.voltageVolts * inputs.currentAmps;
-        inputs.temperatureCelsius = motor.getDeviceTemp().getValueAsDouble();
+    inputs.targetPositionRotations = targetPositionRotations;
+  }
 
-        inputs.targetPositionRotations = targetPositionRotations;
-    }
-
-    public enum PositionControlRequest {
-        DUTYCYCLE,
-        VOLTAGE,
-        TORQUE
-    }
+  public enum PositionControlRequest {
+    DUTYCYCLE,
+    VOLTAGE,
+    TORQUE
+  }
 }
