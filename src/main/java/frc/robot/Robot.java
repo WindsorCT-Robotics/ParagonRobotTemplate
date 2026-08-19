b9e7155dffbec1ab5702cb32ad8d7a9559c3566a
SuppressedCoder <aldwinpham@gmail.com>
2026-08-18 22:15:04 -0400

    Add Bindings for Xbox Controller to manage Trigger combinations
    Bindings queues button combinations that call commands, which only works if you call build() after declaring your binds. By not creating the Trigger in bind(), overlap detection is possible.
    
    When one combination contains another, the debounce and negation is applied to the smaller combination. Debounce prevents a smaller combination from firing by accident. Negation prevents commands from going through after debounce by preventing the trigger from triggering while pressing the other combination.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.robot_container.RobotContainer;
 import frc.robot.util.FullSubsystem;
 
 import org.littletonrobotics.junction.LogFileUtil;
